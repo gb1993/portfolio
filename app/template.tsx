e18871b2ed460163d8e652018b69b02852f9c96d
gabriel branco <gbanco1993@gmail.com>
2025-03-16 18:03:25 -0300

    fix animacao pagina

--- app/template.tsx
+++ app/template.tsx
@@ -2,9 +2,9 @@ import PageTransition from "@/components/PageTransition";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="relative overflow-hidden">
       <PageTransition />
-      <div className="animate-opacity-page">{children}</div>
-    </>
+      <div className="min-h-dvh animate-opacity-page">{children}</div>
+    </div>
   );
 }
--- components/PageTransition/index.tsx
+++ components/PageTransition/index.tsx
@@ -1,6 +1,6 @@
 const PageTransition = () => {
   return (
-    <div className="bg-transparent absolute top-0 left-0 right-0 bottom-0 pointer-events-none overflow-hidden z-20">
+    <div className="bg-transparent h-dvh absolute top-0 left-0 right-0 bottom-0 pointer-events-none overflow-hidden z-20">
       <div className="page-transition-bar bg-primary" />
       <div className="page-transition-bar bg-primary" />
       <div className="page-transition-bar bg-primary" />
