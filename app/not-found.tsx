cef083bdcc4d0f977dd0a231f8d483c7e4976563
gabriel branco <gbanco1993@gmail.com>
2025-04-03 14:09:24 -0300

    feat: 404

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="w-full min-h-dvh bg-dark-or-light-primary flex items-center justify-center animate-opacity-page">
+      <h1 className="text-dark-or-light-secondary text-3xl font-semibold">Nada por aqui 😉</h1>
+    </main>
+  );
+};
+
+export default NotFound;
--- components/FloatBar/index.tsx
+++ components/FloatBar/index.tsx
@@ -22,18 +22,30 @@ import { DrawerProvider } from "@/context/DrawerContext";
 const FloatBar = () => {
   const { setColor, mode, toggleMode } = useColor();
   const pathname = usePathname();
-  const isActive = (href: string) => pathname === href;
+
+  // Função para normalizar o pathname (remove a barra final, se não for a raiz)
+  const normalizePath = (path: string) =>
+    path !== "/" && path.endsWith("/") ? path.slice(0, -1) : path;
+
+  const currentPath = normalizePath(pathname);
+
   const title = [
     { link: "/", text: "HOME", highlight: "" },
     { link: "/sobre", text: "SOBRE", highlight: "MIM" },
     { link: "/trabalhos", text: "MEU", highlight: "PORTFÓLIO" },
     { link: "/contato", text: "FALE", highlight: "COMIGO" },
+    { link: "/*", text: "ESTÁ", highlight: "PERDIDO?" },
   ];
-  const currentTitle = title.find((item) => item.link === pathname);
+
+  const currentTitle =
+    title.find((item) => item.link === currentPath) ||
+    title.find((item) => item.link === "/*");
+
   const headerMobileBG = `${
     mode === "dark" ? "bg-black-secondary" : "bg-white"
   }`;
   const headerMobileCss = `${headerMobileBG} w-full flex items-center justify-between lg:hidden fixed top-0 z-[11] animate-opacity-page py-2 px-4 border-b border-dark-or-light-secondary`;
+
   return (
     <>
       <header className="hidden lg:block fixed top-1/2 right-4 -translate-y-1/2 z-10 animate-opacity-page">
@@ -43,7 +55,7 @@ const FloatBar = () => {
               <h2 className={floatBarItemTitleCss}>HOME</h2>
               <div
                 className={`${iconContentCss} ${
-                  isActive("/") ? "bg-primary" : "bg-black-secondary"
+                  currentPath === "/" ? "bg-primary" : "bg-black-secondary"
                 }`}
               >
                 <HomeIcon sx={{ fontSize: "26px" }} />
@@ -55,7 +67,7 @@ const FloatBar = () => {
               <h2 className={floatBarItemTitleCss}>SOBRE</h2>
               <div
                 className={`${iconContentCss} ${
-                  isActive("/sobre") ? "bg-primary" : "bg-black-secondary"
+                  currentPath === "/sobre" ? "bg-primary" : "bg-black-secondary"
                 }`}
               >
                 <PersonIcon sx={{ fontSize: "28px" }} />
@@ -67,7 +79,9 @@ const FloatBar = () => {
               <h2 className={floatBarItemTitleCss}>TRABALHOS</h2>
               <div
                 className={`${iconContentCss} ${
-                  isActive("/trabalhos") ? "bg-primary" : "bg-black-secondary"
+                  currentPath === "/trabalhos"
+                    ? "bg-primary"
+                    : "bg-black-secondary"
                 }`}
               >
                 <BusinessCenterIcon sx={{ fontSize: "26px" }} />
@@ -79,14 +93,15 @@ const FloatBar = () => {
               <h2 className={floatBarItemTitleCss}>CONTATO</h2>
               <div
                 className={`${iconContentCss} ${
-                  isActive("/contato") ? "bg-primary" : "bg-black-secondary"
+                  currentPath === "/contato"
+                    ? "bg-primary"
+                    : "bg-black-secondary"
                 }`}
               >
                 <EmailIcon sx={{ fontSize: "26px" }} />
               </div>
             </Link>
           </li>
-
           <li className={`group ${floatBarItemCss} flex`}>
             <ul className={floatBarItemTitleCss}>
               {themes.map((theme, index) => (
@@ -105,7 +120,6 @@ const FloatBar = () => {
               />
             </div>
           </li>
-
           <li className={`group ${floatBarItemCss}`} onClick={toggleMode}>
             <div className="flex items-center justify-center w-full h-full z-[2] bg-black-secondary rounded-full group-hover:bg-primary duration-400">
               <DarModeIcon darkMode={mode === "dark"} />
@@ -116,9 +130,9 @@ const FloatBar = () => {
       <header className={headerMobileCss}>
         <h1 className="text-dark-or-light-secondary text-2xl font-extrabold">
           {currentTitle ? currentTitle.text : "HOME"}
-          <span className="text-primary ml-2">
-            {currentTitle ? currentTitle.highlight : ""}
-          </span>
+          {currentTitle && currentTitle.highlight && (
+            <span className="text-primary ml-2">{currentTitle.highlight}</span>
+          )}
         </h1>
         <DrawerProvider>
           <AnimatedHamburgerButton />
